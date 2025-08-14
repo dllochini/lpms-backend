a1c20dbcca7997757696486c955d63f3f18fb268
chathubha <chathubhavithanagamage@gmail.com>
2025-08-14 13:51:34 +0530

    fixed errors in unit

--- src/repositories/unit.js
+++ src/repositories/unit.js
@@ -3,28 +3,33 @@ import Unit from "../models/unit.js";
 
 // Get all units
 export const getAllUnits = async () => {
-  return await Unit.find();
+  const units = await Unit.find();
+  return units;
 };
 
 // Get a single unit by ID
 export const getUnitById = async (unitID) => {
-  return await Unit.findOne({ unitID });
+  const unit = await Unit.findById({ unitID });
+  return unit;
 };
 
 // Create a new unit
 export const createUnit = async (unitData) => {
   const unit = new Unit(unitData);
-  return await unit.save();
+  const newUnit = await unit.save();
+  return newUnit;
 };
 
 // Update a unit by ID
 export const updateUnit = async (unitID, updateData) => {
-  return await Unit.findOneAndUpdate({ unitID }, updateData, { new: true });
+  const updateUnit = await Unit.findByIdAndUpdate({ unitID }, updateData, { new: true });
+  return updateUnit;
 };
 
 // Delete a unit by ID
 export const deleteUnit = async (unitID) => {
-  return await Unit.findOneAndDelete({ unitID });
+ const deleteUnit = await Unit.findByIdAndDelete({ unitID });
+ return deleteUnit;
 };
 
 export default {
