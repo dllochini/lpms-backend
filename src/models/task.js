5aa8f3a79b3da4ea70593997eec84d95db2f20d0
chathubha <chathubhavithanagamage@gmail.com>
2025-09-14 19:57:50 +0530

    updated role , task & workDone models

--- src/models/task.js
+++ src/models/task.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const taskSchema = new Schema({
-   // taskID :String
-   land: {
-    type: Schema.Types.ObjectId,
-    ref: "Land",
-    required: true,
-  },
-    operation:{
-    type: Schema.Types.ObjectId,
-    ref: "Operation",
-    required: true,
-  },
+const taskSchema = new Schema(
+  {
+    processID: {
+      type: Schema.Types.ObjectId,
+      ref: "Process",
+      required: true,
+    },
+    operationID: {
+      type: Schema.Types.ObjectId,
+      ref: "Operation",
+      required: true,
+    },
     start_date: {
       type: Date,
       required: true,
@@ -21,12 +21,39 @@ const taskSchema = new Schema({
       type: Date,
       required: true,
     },
+    end_date: {
+      type: Date,
+    },
     status: {
       type: String,
       required: true,
       maxlength: 500,
       trim: true,
     },
-});
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    updated_at: {
+      type: Date,
+    },
+    updated_by: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    update_history: [
+      {
+        updated_at: Date,
+        updated_by: { type: Schema.Types.ObjectId, ref: "User" },
+        changes: String,
+      },
+    ],
+  },
+  { versionKey: false }
+);
 
 export default mongoose.model("Task", taskSchema, "task");
