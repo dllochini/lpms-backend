9a74b0b7ffd1d2744e84dd5ecb47eef701f9f410
Lochini DIkkumbura <lochidik@gmail.com>
2025-09-17 23:33:14 +0530

    Revert "updated all models"

--- src/routes/role.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRoles } from "../controllers/roleController.js";
-
-const router = express.Router();
-
-router.get("/", getRoles);
-
-export default router;
