517eeec13bcb7db171257046bb12a6a142c09854
Ayesh <manilkafernando2000@gmail.com>
2025-09-30 21:38:08 +0530

    make naming consistent in Role model

--- src/models/role.js
+++ src/models/role.js
@@ -5,14 +5,7 @@ const roleSchema = new Schema(
   {
     role: { type: Schema.Types.ObjectId, auto: true }, // PK
     name: { type: String, required: true },
-
-    created_at: { type: Date, default: Date.now },
-    created_by: { type: Schema.Types.ObjectId }, // not FK, just stored ID
-
-    updated_at: { type: Date, default: Date.now },
-    updated_by: { type: Schema.Types.ObjectId }, // not FK, just stored ID
   },
-  { timestamps: false } // using our own audit fields
 );
 
 export default mongoose.model("Role", roleSchema, "role");
